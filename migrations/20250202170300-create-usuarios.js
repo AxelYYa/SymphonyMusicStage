50274f6e4c2511bf12eb2fd52d5d1982f4c2964b
AxelYYa <axel.y.ya.0@gmail.com>
2025-02-02 17:30:37 -0600

    De migraciones, modelos y otros pormenores.

---
+++ migrations/20250202170300-create-usuarios.js
@@ -0,0 +1,53 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Usuarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      correo: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      contraseña: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      rolId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Roles',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      direccion: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      personaId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Personas',
+          key: 'id'
+        },
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Usuarios');
+  }
+};
---
+++ migrations/20250202170400-create-productos.js
@@ -0,0 +1,44 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Productos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nombre: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      descripcion: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      stock: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      stock_minimo: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      precio: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Productos');
+  }
+};
